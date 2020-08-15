4b02006f215c973e0b18e9b7d12a081de182e793
akhil <carrystartup@gmail.com>
2020-08-15 12:37:13 +0530

    [dev]: Api folder added, logout button added

---
+++ client/app/Api/api.js
@@ -0,0 +1,7 @@
+export const get_logout = '/v1/api/account/logout?token=';
+export const post_sign_in = '/v1/api/account/signin';
+export const post_sign_up = '/v1/api/account/signup';
+export const post_add_course = '/v1/api/courses/add';
+export const get_courses = '/v1/api/courses';
+export const get_verify = '/v1/api/account/verify?token=';
+                           
